494271c23a97ad5cce05e50ba94a1cc321cfb061
diego576 <diegomoreno576@gmail.com>
2022-11-19 22:38:43 +0100

    nueva app

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const APP_URL = "https://notecopies-api.onrender.com/"
-export const APP_CABLE_URL="ws://notecopies-api.onrender.com/cable";
+export const APP_URL = "http://137.74.117.3:3000/"
+export const APP_CABLE_URL="ws://137.74.117.3:3000/cable";
