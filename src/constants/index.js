f49b96f9fc2f40de238117ca406abedaa6550594
Diego Moreno <63602450+diegomoreno576@users.noreply.github.com>
2023-05-15 21:23:43 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const APP_URL = "https://api.notecopies.app"
-export const APP_CABLE_URL="ws://api.notecopies.app/cable";
+export const APP_URL = "http://localhost:3000"
+export const APP_CABLE_URL="ws://localhost:3000/cable";
